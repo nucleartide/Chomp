30cd345f41cc559f1169f91fe68de49e49b92fd5
Jason Tu <jasontu4@gmail.com>
2023-06-19 18:20:29 -0400

    Configure ghosts + pacman so that overlap events are generated

--- Source/KiwiPizza/Private/GhostPawn.cpp
+++ Source/KiwiPizza/Private/GhostPawn.cpp
@@ -1,4 +1,5 @@
 #include "GhostPawn.h"
+#include "Debug.h"
 
 AGhostPawn::AGhostPawn()
 {
@@ -8,6 +9,15 @@ AGhostPawn::AGhostPawn()
 void AGhostPawn::BeginPlay()
 {
 	Super::BeginPlay();
+
+	auto HeadComponents = GetComponentsByTag(UStaticMeshComponent::StaticClass(), FName("Head"));
+	check(HeadComponents.Num() == 1);
+
+	auto BodyComponents = GetComponentsByTag(UStaticMeshComponent::StaticClass(), FName("Body"));
+	check(BodyComponents.Num() == 1);
+
+	DEBUG_LOG(TEXT("Head component name: %s"), *HeadComponents[0]->GetReadableName());
+	DEBUG_LOG(TEXT("Body component name: %s"), *BodyComponents[0]->GetReadableName());
 }
 
 void AGhostPawn::Tick(float DeltaTime)
--- Source/KiwiPizza/Private/GhostPawn.h
+++ Source/KiwiPizza/Private/GhostPawn.h
@@ -18,18 +18,9 @@ protected:
 
 public:	
 	virtual void Tick(float DeltaTime) override;
-
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Component References")
+private:
 	class UStaticMeshComponent* HeadStaticMesh;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Component References")
 	class UStaticMeshComponent* BodyStaticMesh;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Level Loader Customization")
-	FLinearColor Color;
-
-	// color to set the ghost to
-	// on beginplay, both meshes get set to the specified color
 };
