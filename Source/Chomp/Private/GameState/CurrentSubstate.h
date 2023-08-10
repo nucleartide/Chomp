a16e686010f0e02461400d9f1f21a192068d56d2
Jason Tu <jasontu4@gmail.com>
2023-08-10 18:35:51 -0400

    Add some pre- and post- assertions

--- Source/Chomp/Private/GameState/CurrentSubstate.h
+++ Source/Chomp/Private/GameState/CurrentSubstate.h
@@ -14,18 +14,18 @@ struct FCurrentSubstate
 	GENERATED_BODY()
 
 private:
-	// The substate that the game was in last frame.
+	// The substate that the game was in last tick.
 	UPROPERTY(VisibleInstanceOnly)
 	EChompPlayingSubstateEnum LastPlayingSubstate = EChompPlayingSubstateEnum::None;
 
 	// The time reported by UWorld::GetTimeSeconds when the game entered into an EChompGameStateEnum::Playing state.
 	UPROPERTY(VisibleInstanceOnly)
-	double PlayingStartTime = std::numeric_limits<double>::max();
+	double PlayingStartTime = 0.0;
 
 	// The time reported by UWorld::GetTimeSeconds when the game entered into an
 	// EChompPlayingSubstateEnum::Frightened substate, during which Pacman has the opportunity to consume ghosts.
 	UPROPERTY(VisibleInstanceOnly)
-	double FrightenedStartTime = std::numeric_limits<double>::max();
+	double FrightenedStartTime = 0.0;
 
 	// The accrued time spent in the frightened substate.
 	//
@@ -36,15 +36,30 @@ private:
 
 	// Reference to the configured WavesRef of ghost behavior.
 	//
-	// Note: this value should be injected by constructor.
+	// Note: this value should be injected by constructor, and should not be mutated once set.
 	TArray<FWave> WavesRef;
 
-	// Reference to frightened substate duration.
+	// Reference to duration of time to spend in frightened substate.
 	//
-	// Note: this value should be injected by constructor.
+	// Note: this value should be injected by constructor, and should not be mutated once set.
 	double FrightenedDurationRef = 0.0;
 
-	EChompPlayingSubstateEnum GetCurrentPlayingSubstate(const double CurrentWorldTimeSeconds) const
+public:
+	[[nodiscard]] FCurrentSubstate()
+	{
+	}
+
+	[[nodiscard]] FCurrentSubstate(
+		const TArray<FWave>& WavesRef,
+		const double FrightenedDurationRef
+	):
+		FrightenedStartTime(-FrightenedDurationRef),
+		WavesRef(WavesRef),
+		FrightenedDurationRef(FrightenedDurationRef)
+	{
+	}
+
+	EChompPlayingSubstateEnum GetEnum(const double CurrentWorldTimeSeconds) const
 	{
 		// Early return if we are in a frightened substate.
 		if (CurrentWorldTimeSeconds < FrightenedStartTime + FrightenedDurationRef)
@@ -79,44 +94,52 @@ private:
 		return EChompPlayingSubstateEnum::None;
 	}
 
-public:
-	[[nodiscard]] FCurrentSubstate()
-	{
-	}
-
-	[[nodiscard]] FCurrentSubstate(
-		const TArray<FWave>& WavesRef,
-		const double FrightenedSubstateDurationRef
-	):
-		FrightenedStartTime(-FrightenedSubstateDurationRef),
-		WavesRef(WavesRef),
-		FrightenedDurationRef(FrightenedSubstateDurationRef)
-	{
-	}
-
 	void StartGame(const double CurrentWorldTimeSeconds)
 	{
-		check(GetCurrentPlayingSubstate(CurrentWorldTimeSeconds) != EChompPlayingSubstateEnum::Frightened);
+		// Pre-conditions.
+		check(GetEnum(CurrentWorldTimeSeconds) != EChompPlayingSubstateEnum::Frightened);
+
+		LastPlayingSubstate = EChompPlayingSubstateEnum::None;
 		PlayingStartTime = CurrentWorldTimeSeconds;
+		FrightenedStartTime = -FrightenedDurationRef;
+		TimeSpentInFrightenedSubstate = 0.0;
+
+		// Post-conditions.
+		check(GetEnum(CurrentWorldTimeSeconds) != EChompPlayingSubstateEnum::Frightened);
 	}
 
-	// We keep a reference to the LastPlayingSubstate so that we can call a delegate when the substate changes.
+	// Note: we keep a reference to the LastPlayingSubstate so that we can call a delegate when the substate changes.
 	std::tuple<EChompPlayingSubstateEnum, EChompPlayingSubstateEnum> Tick(const double CurrentWorldTimeSeconds)
 	{
 		// Compute the current substate.
-		const auto CurrentSubstate = GetCurrentPlayingSubstate(CurrentWorldTimeSeconds);
+		const auto CurrentSubstate = GetEnum(CurrentWorldTimeSeconds);
 
 		// Grab a reference to LastPlayingSubstate. We're about to overwrite it.
 		const auto OldLastPlayingSubstate = LastPlayingSubstate;
 
+		// Update TimeSpentInFrightenedState if we are transitioning out of frightened state.
+		if (OldLastPlayingSubstate == EChompPlayingSubstateEnum::Frightened &&
+			CurrentSubstate != EChompPlayingSubstateEnum::Frightened)
+		{
+			TimeSpentInFrightenedSubstate += FrightenedDurationRef;
+		}
+
 		// Assign LastPlayingSubstate to the current substate.
 		LastPlayingSubstate = CurrentSubstate;
+
+		// Post-conditions.
+		check(
+			OldLastPlayingSubstate == EChompPlayingSubstateEnum::Frightened
+			&& CurrentSubstate != EChompPlayingSubstateEnum::Frightened
+			? TimeSpentInFrightenedSubstate >= FrightenedDurationRef
+			: true
+		);
 
 		// Return the current substate and reference to old substate to the caller.
 		return std::make_tuple(OldLastPlayingSubstate, CurrentSubstate);
 	}
 
-	std::tuple<EChompPlayingSubstateEnum, EChompPlayingSubstateEnum> StopPlaying()
+	std::tuple<EChompPlayingSubstateEnum, EChompPlayingSubstateEnum> StopPlaying(const double CurrentWorldTimeSeconds)
 	{
 		// We're switching to the "None" substate.
 		constexpr auto CurrentSubstate = EChompPlayingSubstateEnum::None;
@@ -124,26 +147,49 @@ public:
 		// Grab a reference to LastPlayingSubstate. We're about to overwrite it.
 		const auto OldLastPlayingSubstate = LastPlayingSubstate;
 
+		// Update TimeSpentInFrightenedState if we are transitioning out of frightened state.
+		if (OldLastPlayingSubstate == EChompPlayingSubstateEnum::Frightened)
+		{
+			TimeSpentInFrightenedSubstate += CurrentWorldTimeSeconds - FrightenedStartTime;
+		}
+
 		// Assign LastPlayingSubstate to the current substate.
 		LastPlayingSubstate = CurrentSubstate;
 
+		// Post-conditions.
+		checkf(
+			OldLastPlayingSubstate != CurrentSubstate,
+			TEXT("If we are not playing, you should not be calling StopPlaying() a second time.")
+		);
+		check(
+			OldLastPlayingSubstate == EChompPlayingSubstateEnum::Frightened
+			? TimeSpentInFrightenedSubstate >= CurrentWorldTimeSeconds - FrightenedStartTime
+			: true
+		);
+
 		// Return the current substate and reference to old substate to the caller.
-		check(OldLastPlayingSubstate != CurrentSubstate);
 		return std::make_tuple(OldLastPlayingSubstate, CurrentSubstate);
 	}
 
-	EChompPlayingSubstateEnum GetEnum(const double CurrentWorldTimeSeconds) const
+	void Frighten(const double CurrentWorldTimeSeconds)
 	{
-		return GetCurrentPlayingSubstate(CurrentWorldTimeSeconds);
-	}
+		// If we *are* (present tense) in the Frightened substate,
+		const auto IsCurrentlyFrightened = GetEnum(CurrentWorldTimeSeconds) == EChompPlayingSubstateEnum::Frightened;
+		const auto OldFrightenedStartTime = FrightenedStartTime;
+		if (IsCurrentlyFrightened)
+		{
+			// Add CurrentWorldTimeSeconds - FrightenedStartTime to the TimeSpentInFrightenedSubstate.
+			TimeSpentInFrightenedSubstate += CurrentWorldTimeSeconds - OldFrightenedStartTime;
+		}
 
-#if false
-	// TODO(2): Do this after the existing game features have been refactored to use this struct.
-	// TODO: You will need to update the behavior of Tick() to take into account this Frightened interruption.
-	void Frighten(double CurrentWorldTimeInSeconds)
-	{
-		// Enter the Frightened substate.
-		// ...
+		// Set the new frightened start time.
+		FrightenedStartTime = CurrentWorldTimeSeconds;
+
+		// Post-conditions.
+		check(
+			IsCurrentlyFrightened
+			? TimeSpentInFrightenedSubstate >= CurrentWorldTimeSeconds - OldFrightenedStartTime
+			: false
+		);
 	}
-#endif
 };
