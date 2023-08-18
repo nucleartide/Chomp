1d5188b795d9d22f5a54d4defda36147d56f3c93
Jason Tu <jasontu4@gmail.com>
2023-08-18 17:09:38 -0400

    Configure coroutine lib

--- Source/Chomp.Target.cs
+++ Source/Chomp.Target.cs
@@ -11,5 +11,6 @@ public class ChompTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 		ExtraModuleNames.Add("Chomp");
+        bEnableCppCoroutinesForEvaluation = true;
 	}
 }
