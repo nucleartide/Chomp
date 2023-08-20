987285c89064162871750ab72f003d9838ceb47f
Jason Tu <jasontu4@gmail.com>
2023-08-20 10:18:46 -0400

    Format Build + Target files

--- Source/ChompEditor.Target.cs
+++ Source/ChompEditor.Target.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 public class ChompEditorTarget : TargetRules
 {
-	public ChompEditorTarget( TargetInfo Target) : base(Target)
+	public ChompEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 		ExtraModuleNames.Add("Chomp");
-		
+
 		// Get UE5Coro to compile
 		bEnableCppCoroutinesForEvaluation = true;
 		CppStandard = CppStandardVersion.Cpp20;
