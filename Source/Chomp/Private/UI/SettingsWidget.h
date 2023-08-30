42423fc6b00c4a23a324a9fc02da817e0d8e0cf6
Jason Tu <jasontu4@gmail.com>
2023-08-30 12:02:48 -0400

    Add initial markup for settings widget

---
+++ Source/Chomp/Private/UI/SettingsWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SettingsWidget.generated.h"
+
+UCLASS()
+class CHOMP_API USettingsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
